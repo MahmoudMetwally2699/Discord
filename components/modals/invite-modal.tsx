a144c210be2e494582a66234bf53f010eaa0dc7c
MahmoudMetwally2699 <113688967+MahmoudMetwally2699@users.noreply.github.com>
2024-05-04 12:22:30 +0300

    Update invite-modal.tsx

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -62,7 +62,7 @@ export const InviteModal = () => {
           <Label
             className="uppercase text-xs font-bold text-zinc-500 dark:text-secondary/70"
           >
-            Server invite link
+            Community invite link
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
             <Input
