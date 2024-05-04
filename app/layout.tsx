052807556439399b69832e91e9b9d45dc8740768
MahmoudMetwally2699 <113688967+MahmoudMetwally2699@users.noreply.github.com>
2024-05-04 12:37:15 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,7 +12,7 @@ import { QueryProvider } from '@/components/providers/query-provider'
 const font = Open_Sans({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Discord ',
+  title: 'I-Talk ',
   description: 'Created By Team forever',
 }
 
