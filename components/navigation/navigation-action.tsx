cbe6cc3969d8c4da6d908ba685523fb4d9fe35aa
MahmoudMetwally2699 <113688967+MahmoudMetwally2699@users.noreply.github.com>
2024-05-04 12:30:36 +0300

    Update navigation-action.tsx

--- components/navigation/navigation-action.tsx
+++ components/navigation/navigation-action.tsx
@@ -13,7 +13,7 @@ export const NavigationAction = () => {
       <ActionTooltip
         side="right"
         align="center"
-        label="Add a server"
+        label="Add a Community"
       >
         <button
           onClick={() => onOpen("createServer")}
