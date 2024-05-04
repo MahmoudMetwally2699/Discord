cf17af1f35eeea84f6afbed43bf66b95496eebc4
MahmoudMetwally2699 <113688967+MahmoudMetwally2699@users.noreply.github.com>
2024-05-04 12:19:59 +0300

    Update delete-server-modal.tsx

--- components/modals/delete-server-modal.tsx
+++ components/modals/delete-server-modal.tsx
@@ -45,7 +45,7 @@ export const DeleteServerModal = () => {
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
         <DialogHeader className="pt-8 px-6">
           <DialogTitle className="text-2xl text-center font-bold">
-            Delete Server
+            Delete Community
           </DialogTitle>
           <DialogDescription className="text-center text-zinc-500">
             Are you sure you want to do this? <br />
